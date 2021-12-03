6cf8fb3ea44c1c037d330c61595812be9fe36134
Fadhel Ijlal Falah <fadhel.ijlal@nhsec.co.id>
2021-12-03 09:40:15 +0700

    display map

--- components/Map/index.jsx
+++ components/Map/index.jsx
@@ -9,8 +9,8 @@ import { useState } from "react";
 
 export default function Map(){
     const [state,setstate] = useState({
-        lat:115.21793,
-        lng:-8.66908
+        lat:106.79855,
+        lng:-6.21853,
     })
     const apiKey = "BYrIifYEk_cazqQRnTgSzrkZcrq7UyvsF4ZPGTgg0fQ"
   const onChange = async(e) => {
@@ -22,9 +22,8 @@ export default function Map(){
     setstate(
         prev=>({
         ...prev,
-        ...res.items[0].position
+        ...((res.items)?(res.items[0].position):state)
     }));
-    console.log({...res.items[0].position})
   };
 
   const onBlur = () => {
@@ -38,6 +37,8 @@ export default function Map(){
   const onSearch = (val) => {
     console.log("search:", val);
   };
+  
+  console.log(state)
     return(
         <>
         <DashboardLayout>
@@ -62,21 +63,19 @@ export default function Map(){
           </Row>
         </Form>
         <MapContainer
-        center={[state.lng, state.lat]}
+        center={[state.lat, state.lng]}
         zoom={14}
         scrollWheelZoom={false}
         style={{ height: "100%", width: "100%" }}
       >
         {
-          ()=>{
-            console.log(state.lng, state.lat)
-          }
+          console.log("change :",state.lat, state.lng)
         }
         <TileLayer
-          url={`https://2.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/reduced.night/{z}/{x}/{y}/512/png8?apiKey=${apiKey}&ppi=320`}
+          url={`https://2.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/512/png8?apiKey=${apiKey}&ppi=320`}
           attribution='Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
         />
-        <Marker position={[state.lng, state.lat]} draggable={true} animate={true}>
+        <Marker position={[state.lat, state.lng]} draggable={true} animate={true}>
           <Popup>Hey ! I live here</Popup>
         </Marker>
       </MapContainer>
