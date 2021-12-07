b4ef9f7418132995999a54470a14f7b85f109a2d
Fadhel Ijlal Falah <fadhel.ijlal@nhsec.co.id>
2021-12-07 10:51:13 +0700

    static

---
+++ _static/_next/static/uR1ylLAaccnGY7_ODzQmm/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fanggota","\u002Fdashboard\u002Fjadwal","\u002Fdashboard\u002Fjadwal\u002Ftambah","\u002Fdashboard\u002Fpendaftaran","\u002Fdashboard\u002Fobjek-wisata"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
