f01c703c13f5f4009242520914556427af6c3df9
Fadhel Ijlal Falah <fadhel.ijlal@nhsec.co.id>
2021-12-07 11:15:28 +0700

    publish heroku

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
     reactStrictMode: true,
-    tourify:'http://localhost:3000/api',
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+    tourify:'http://localhost:3000/api'
 }
