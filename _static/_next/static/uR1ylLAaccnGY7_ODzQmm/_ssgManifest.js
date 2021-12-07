9a6cd58e4eb3d68fe712818fcb3aca69ddf96a60
Fadhel Ijlal Falah <fadhel.ijlal@nhsec.co.id>
2021-12-07 10:57:40 +0700

    Add github Actions

--- _static/_next/static/uR1ylLAaccnGY7_ODzQmm/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fanggota","\u002Fdashboard\u002Fjadwal","\u002Fdashboard\u002Fjadwal\u002Ftambah","\u002Fdashboard\u002Fpendaftaran","\u002Fdashboard\u002Fobjek-wisata"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
